a04e179803774476e3f5a7dd7473e42950f6f8fc
Piramid Surin <marlovobook@hotmail.com>
2023-05-12 11:40:28 +0700

    Determine ROP

--- src/feature-forecast.py
+++ src/feature-forecast.py
@@ -22,3 +22,21 @@ import scipy.stats as stats
 from google.cloud import bigquery
 import pandas_gbq
 import random
+
+#Get Forecast result and past data to determine ROP from BQ¶
+
+client = bigquery.Client()
+sql = """
+SELECT
+    FORMAT_TIMESTAMP('%Y-%m-%d', CAST(ds AS TIMESTAMP)) AS ds,
+    shop_id AS shop_id,
+    material_name AS material_name,
+    y AS y,
+    yhat AS yhat
+FROM datacafeplayground.total_supply_chain.table_forecast_material
+ORDER BY
+  ds,shop_id,material_name;
+"""
+result= client.query(sql).to_dataframe()
+
+result.head()
